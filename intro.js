8b51b292cf7727e929f9f5d4d94fff0732a6e5da
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-16 12:22:20 -0500

    Implement task to change heading when hovering over items

--- intro.js
+++ intro.js
@@ -2,23 +2,37 @@
 	var items = document.getElementById('thelist');
 	var button = document.getElementById('add-btn');
 
+    var heading = document.getElementById('h');
+
+    var attachTextChangeEvents = function (elem) {
+        elem.addEventListener('mouseenter', function () {
+            heading.innerHTML = elem.innerHTML;
+        });
+        elem.addEventListener('mouseleave', function () {
+            heading.innerHTML = "Hello, world!";
+        });
+    };
+
+    for (var li of items.children) {
+        attachTextChangeEvents(li);
+    }
+
 	button.addEventListener('click', function () {
 		var item = document.createElement('li');
 		item.innerHTML = 'New Thing!';
+        attachTextChangeEvents(item);
 		items.appendChild(item);
 	});
 
-	items.addEventListener('click', function (e) {
+    items.addEventListener('click', function (e) {
         if (e.target.tagName == 'LI') {
             e.target.remove();
         }
-	});
+    });
 
     var greenDiv = document.getElementById('div1');
     var blackDiv = document.getElementById('div2');
     var blueDiv = document.querySelector('div.blue');
-
-    var heading = document.getElementById('h');
 
     greenDiv.addEventListener('mouseenter', function () {
         heading.setAttribute('class', 'green');
